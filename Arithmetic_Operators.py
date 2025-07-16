0fbf67fa35eca1143d563324019dbd25626a14da
prasath-m21 <mprasath0907@gmail.com>
2025-07-16 11:56:37 +0530

    Initial commit with HackerRank solution files

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,6 @@
+a = int(input("Enter a Number a "))
+b = int(input("Enter a Number b "))
+print(a+b)
+print(a-b)
+print(a*b)
+2
---
+++ Loops.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number "))
+lst = []
+for i in range(n):
+    if i < n:
+        squ = print(i*i)
+        lst.append(squ)
+                   
